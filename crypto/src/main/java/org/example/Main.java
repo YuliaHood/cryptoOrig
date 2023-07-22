6445c664fb0d15a55196aea0f5b69e2d53b7f6e5
Yulia <uliahoodzov@gmail.com>
2023-07-22 23:58:10 +0300

    second step + keyUser

--- crypto/src/main/java/org/example/Main.java
+++ crypto/src/main/java/org/example/Main.java
@@ -9,14 +9,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean isValidPath = false;
 
         System.out.println("Добрий день! Введіть будь ласка шлях до вашохо файлу");
         String path = scanner.nextLine();
-        if (isValidFilePath(path)) {
-            System.out.println("Путь к файлу верный.");
-        } else {
-            System.out.println("Путь к файлу неверный.");
+
+        while(!isValidPath) {
+            if (isValidFilePath(path)) {
+                isValidPath = true;
+            }else {
+                System.out.println("Путь до файлу не вірний( спробуйте будь ласка ще раз!");
+            }
         }
+
+        System.out.println("Тепер введіть ключ " + "(Обовязково число!)");
+        char userInput = scanner.next().charAt(0);
+
+        if (!Character.isDigit(userInput)) {
+            System.out.println("Введіть будь ласка саме число!");
+        }
+        int key = (int) userInput;
+
+
 
     }
     public static boolean isValidFilePath(String filePath) {
@@ -27,4 +41,5 @@ public class Main {
             return false;
         }
     }
+
 }
